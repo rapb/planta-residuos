050b37a21ce1966a32e99038e4222d74be923ab3
arrey <arrey@yo>
2018-04-20 19:52:46 +0200

    clase vehiculo

--- plantaResiduos/src/plantaresiduos/Vehiculo.java
+++ plantaResiduos/src/plantaresiduos/Vehiculo.java
@@ -16,8 +16,8 @@ public class Vehiculo {
     private String matricula,modelo;
     private int numeroAñosOperativo,estado;
     private boolean disponible;
-    private TCamionero
-    private  HashSet<TCamoinero>conductores ;
+  //  private TCamionero;
+   // private  HashSet<TCamoinero>conductores ;
     public Vehiculo(String matricula, String modelo, int numeroAñosOperativo, int estado, boolean disponible) {
         this.matricula = matricula;
         this.modelo = modelo;
@@ -26,13 +26,13 @@ public class Vehiculo {
         this.disponible = disponible;
     }
     public void conducir(){
-        if()
+    //    if()
         disponible=true;
     }
-    public boolean anhadirConcuctor(TCamoinero conductor){
+   //public boolean anhadirConcuctor(TCamoinero conductor){
         
-    }
+  //  }
     public boolean generarVehiculo(){
-      co
+      return true;
     }
 }
