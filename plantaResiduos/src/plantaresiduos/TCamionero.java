d89cf46e003c28ad22e49c8dc0d8911b0387bdbc
arrey <arrey@yo>
2018-04-20 19:45:31 +0200

    clase camionero

---
+++ plantaResiduos/src/plantaresiduos/TCamionero.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package plantaresiduos;
+
+import java.util.HashSet;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class TCamionero {
+public class TCamoinero {
+private boolean senior;
+  private  HashSet<Vehiculo>vehiculos ;
+    public TCamoinero(boolean senior) {
+        this.senior = senior;
+    }
+
+    public TCamoinero() {
+    }
+public boolean asignaCamion(Vehiculo camion){
+      
+}
+}
+
+}
