c61c8862fa8ebbefd48443951c25e87a718c29f0
Paula <DAW111@IF01-07.iesmhp.local>
2018-04-27 19:11:34 +0200

    Se añade un ArrayList de almacenes. Añadido el método crearAlmacen para crear almacenes y añadirlos a la lista.

--- plantaResiduos/src/plantaresiduos/PlantaResiduos.java
+++ plantaResiduos/src/plantaresiduos/PlantaResiduos.java
@@ -5,15 +5,35 @@
  */
 package plantaresiduos;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author DAW121
  */
 public class PlantaResiduos {
 
-    /**
-     * @param args the command line arguments
-     */
+    private ArrayList<Almacen> almacenes;
+    
+    Scanner Entrada=new Scanner(System.in);
+    public PlantaResiduos() {
+        almacenes=new ArrayList<>();
+    }
+    
+    public void crearAlmacen(){
+        System.out.println("Introduzca el id del almacen:");
+        int id=Entrada.nextInt();
+        System.out.println("Introduzca la capacidad del almacen:");
+        int capa=Entrada.nextInt();
+        System.out.println("Introduzca el nombre del almacen:");
+        String nombre=Entrada.nextLine();
+        System.out.println("Introduzca la descripcion del almacen:");
+        String desc=Entrada.nextLine();
+        Almacen a=new Almacen(id, capa, nombre, desc);
+        almacenes.add(a);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
