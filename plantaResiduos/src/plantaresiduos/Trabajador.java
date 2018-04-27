3c4aec876b39c2328314606cb7b9013cd972567f
Paula <DAW111@IF01-07.iesmhp.local>
2018-04-27 19:02:51 +0200

    Creada la Clase Padre Obrero

---
+++ plantaResiduos/src/plantaresiduos/Trabajador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantaresiduos;
+
+/**
+ *
+ * @author DAW111
+ */
+public class Trabajador {
+    
+    private String nif, nombre, apellidos,telefono, direccion;
+    
+    
+    public Trabajador(String nif, String nombre, String apellidos, String telefono, String direccion) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    
+    
+}
