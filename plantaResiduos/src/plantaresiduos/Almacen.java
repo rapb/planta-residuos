c2fa22a71338bf4eb77df916e4caf3a12b938876
Paula <>
2018-04-27 18:56:58 +0200

    Añadida Clase Trabajador - 1.0 Añadida Clase TOficina Añadida Clase TObrero Añadida Interfaz Reparación Implementada Reparación en TObrero Extendida Trabajador en Clase TCamionero Añadida Clase Almacen

---
+++ plantaResiduos/src/plantaresiduos/Almacen.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantaresiduos;
+
+import java.util.LinkedHashSet;
+
+/**
+ *
+ * @author DAW111
+ */
+public class Almacen {
+    
+    private LinkedHashSet<Residuo> residuos;
+
+    public Almacen() {
+        residuos=new LinkedHashSet<>();
+    }
+    
+    
+}
