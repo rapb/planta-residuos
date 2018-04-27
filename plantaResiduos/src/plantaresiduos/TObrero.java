c647b2e0cd92d817404a0d1152b5c3fa267b9f67
Paula <DAW111@IF01-07.iesmhp.local>
2018-04-27 19:02:02 +0200

    Creada Clase TObrero(extiende Trabajador / Implementa Raparacion)

---
+++ plantaResiduos/src/plantaresiduos/TObrero.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantaresiduos;
+
+/**
+ *
+ * @author DAW111
+ */
+public class TObrero extends Trabajador implements Reparacion{
+    
+    public TObrero(String nif, String nombre, String apellidos, String telefono, String direccion) {
+        super(nif, nombre, apellidos, telefono, direccion);
+    }
+
+    @Override
+    public void reparar() {
+        System.out.println("Introduzca el id del residuo:");
+            
+        
+        }
+            
+        }
+    
+    
+
