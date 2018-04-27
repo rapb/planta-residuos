bfffb733da546891bc045e260845cd5465cad1a2
Paula <DAW111@IF01-07.iesmhp.local>
2018-04-27 19:17:08 +0200

    Se añade un LinkedHashSet de Residuos. Añadido el método anadirResiduo para crear residuos y añadirlos a la lista.

--- plantaResiduos/src/plantaresiduos/Almacen.java
+++ plantaResiduos/src/plantaresiduos/Almacen.java
@@ -5,18 +5,39 @@
  */
 package plantaresiduos;
 
+import java.io.IOException;
 import java.util.LinkedHashSet;
+import java.util.Scanner;
 
 /**
  *
  * @author DAW111
  */
 public class Almacen {
+     Scanner Entrada=new Scanner(System.in);
     
+    private int id, capacidad;
+    private String nombre, ubicacion;
     private LinkedHashSet<Residuo> residuos;
 
-    public Almacen() {
-        residuos=new LinkedHashSet<>();
+    public Almacen(int id, int capacidad, String nombre, String ubicacion) {
+        this.id = id;
+        this.capacidad = capacidad;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+         residuos=new LinkedHashSet<>();
+    }
+
+    public void anadirResiduo() throws IOException{
+        System.out.println("Introduzca el id del residuo:");
+        int id=Entrada.nextInt();
+        System.out.println("Introduzca el estado del residuo:");
+        char estado=(char)System.in.read();
+        System.out.println("Introduzca la descripción del residuo:");
+        String desc=Entrada.nextLine();
+        Residuo r=new Residuo(id, estado, desc);
+        
+        residuos.add(r);
     }
     
     
