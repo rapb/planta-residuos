d48ea9270cb08a1afecbed7aac4ec3e5037892e2
Paula <Paula@192.168.0.160>
2018-05-15 12:19:49 +0200

    Editado reparar porque faltaban algunas cosas. Sigue habiendo que corregir el foreach de la lista de residuos. Además falta añadir el add para lista de reparaciones, una vez ésta esté hecha.

--- plantaResiduos/src/plantaresiduos/TObrero.java
+++ plantaResiduos/src/plantaresiduos/TObrero.java
@@ -5,6 +5,9 @@
  */
 package plantaresiduos;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 /**
  *
  * @author DAW111
@@ -12,6 +15,7 @@ package plantaresiduos;
 public class TObrero extends Trabajador{
     
     private boolean esSupervisor;
+    private int cont=1;
 
     public TObrero(String nif, String nombre, String apellidos, String telefono, String direccion, boolean esSupervisor) {
         super(nif, nombre, apellidos, telefono, direccion);
@@ -25,6 +29,12 @@ public class TObrero extends Trabajador{
         for  (Almacen.Residuo residuo : residuos {
             if(id==residuo.getId()){
                 residuo.setEstado('R');
+                Calendar f=Calendar.getInstance(Locale.ITALY);
+                System.out.println("Introduzca la descripción de la reparación:");
+                String d=Entrada.nextLine();
+                Reparacion r=new Reparacion(cont,f,d);
+                //Aquí hay que poner el add para la lista de reparaciones.
+                
             }
         }
     }
