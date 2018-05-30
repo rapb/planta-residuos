fc0d3e12c18bd5854fe70a1621d53789937156e5
Paula <Paula@192.168.0.160>
2018-05-30 12:36:07 +0200

    Documentación y arreglos varios.

--- plantaResiduos/src/plantaresiduos/Taller.java
+++ plantaResiduos/src/plantaresiduos/Taller.java
@@ -9,25 +9,35 @@ import java.util.LinkedHashSet;
 
 /**
  *
- * @author DAW121
+ * @author Raquel Pérez, Sergio Arrey y Paula Estrada. Desarrollo de aplicaciones Web 1.
  */
 public class Taller {
     
     private String nombre;
     private int maxResido[]=new int [100];
     private LinkedHashSet<Reparacion> reparacion;
-
-    public Taller(String nombre, LinkedHashSet<Reparacion> reparacion) {
+    
+    /**
+     * Constructor
+     * @param nombre - del taller. 
+     * NOTA DE PAULA : Según el manual se identifican los talleres mediante un nombre. ¿No sería más últil un identificador no alfanumérico, sólo numérico?
+     * Se inicializa una lista de reparaciones con él.
+     */
+    public Taller(String nombre) {
         this.nombre = nombre;
-        this.reparacion = reparacion;
+        this.reparacion = new LinkedHashSet<>();
     }
     
+    /**
+     * Método para realizar reparaciones en los residuos. Se obtiene la lista de residuos se busca el que corresponda con el id pasado y se le asigna el estado F(Final)
+     * @param id - id del residuo a reparar.
+     */
     public void reparar(int id){
         LinkedHashSet<Residuo> r;
         r=Almacen.getResiduos;
         for (Residuo residuo : r) {
             if(id==residuo.getId()){
-                residuo.setEstado('R');
+                residuo.setEstado('F');
             }
         }
     }
