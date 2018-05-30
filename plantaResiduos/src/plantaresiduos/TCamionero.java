6df9315604d6b55873b0bb29d4ebf41e519d280b
Paula <Paula@192.168.0.160>
2018-05-30 12:35:16 +0200

    Documentación y arreglos varios.

--- plantaResiduos/src/plantaresiduos/TCamionero.java
+++ plantaResiduos/src/plantaresiduos/TCamionero.java
@@ -10,19 +10,33 @@ import java.util.HashSet;
 
 /**
  * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
+ * @author Raquel Pérez, Sergio Arrey y Paula Estrada. Desarrollo de aplicaciones Web 1.
  */
 public class TCamionero extends Trabajador {
     
     private boolean senior;
     private  HashSet<Vehiculo>vehiculos ;
-
+    
+    /**
+     * Constructor.
+     * Esta clase hereda de Trabajador.
+     * 
+     * @param senior - True si es un conductor senior, false si no.
+     * @param nif - nif del trabajador. Heredado de Trabajador.
+     * @param nombre - nombre del trabajador. Heredado de Trabajador.
+     * @param apellidos - apellidos del trabajador. Heredado de Trabajador.
+     * @param telefono - teleono del trabajador. Heredado de Trabajador.
+     * @param direccion - dirección del trabajador. Heredado de Trabajador.
+     */
     public TCamionero(boolean senior, String nif, String nombre, String apellidos, String telefono, String direccion) {
         super(nif, nombre, apellidos, telefono, direccion);
         this.senior = senior;
     }
     
-
+    /**
+     * EN PROCESO
+     * @param camion 
+     */
     public void asignaCamion(Vehiculo camion){
       
     }
