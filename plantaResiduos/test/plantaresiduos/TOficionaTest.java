c3b21804d9dfee79c4139a457b0bfeb5e02b77f9
Paula <DAW111@IF01-07.iesmhp.local>
2018-06-01 15:36:27 +0200

    Test

---
+++ plantaResiduos/test/plantaresiduos/TOficionaTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantaresiduos;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DAW111
+ */
+public class TOficionaTest {
+    
+    public TOficionaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of generarNomina method, of class TOficiona.
+     */
+    @Test
+    public void testGenerarNomina() {
+        System.out.println("generarNomina");
+        int id = 0;
+        int mes = 0;
+        int anio = 0;
+        int cant = 0;
+        TOficiona instance = null;
+        instance.generarNomina(id, mes, anio, cant);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
