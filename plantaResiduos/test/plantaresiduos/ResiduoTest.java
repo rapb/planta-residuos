f46a732d8e9b8037fe378805919360655658f5f7
Paula <DAW111@IF01-07.iesmhp.local>
2018-06-01 15:36:16 +0200

    Test

---
+++ plantaResiduos/test/plantaresiduos/ResiduoTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantaresiduos;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DAW111
+ */
+public class ResiduoTest {
+    
+    public ResiduoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setEstado method, of class Residuo.
+     */
+    @Test
+    public void testSetEstado() {
+        System.out.println("setEstado");
+        char estado = ' ';
+        Residuo instance = new Residuo();
+        instance.setEstado(estado);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getId method, of class Residuo.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Residuo instance = new Residuo();
+        int expResult = 0;
+        int result = instance.getId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Residuo.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Residuo instance = new Residuo();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
