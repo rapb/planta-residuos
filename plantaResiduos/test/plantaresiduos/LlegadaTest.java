66e87afcbb1e5c709948e41108fb6417d390212f
Paula <DAW111@IF01-07.iesmhp.local>
2018-06-01 15:35:55 +0200

    Test

---
+++ plantaResiduos/test/plantaresiduos/LlegadaTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantaresiduos;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DAW111
+ */
+public class LlegadaTest {
+    
+    public LlegadaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of registrarLlegada method, of class Llegada.
+     */
+    @Test
+    public void testRegistrarLlegada() {
+        System.out.println("registrarLlegada");
+        int id = 0;
+        Date fecha = null;
+        Llegada.registrarLlegada(id, fecha);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
