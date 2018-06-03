45c65b47d0e8df9a3e7ab0140cdddf09fefe4db1
rapb <>
2018-06-03 17:30:24 +0200

    Cambio de nombre TOficina (ponía TOficiona)

---
+++ plantaResiduos/src/plantaresiduos/TOficina.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantaresiduos;
+
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Raquel Pérez, Sergio Arrey y Paula Estrada. Desarrollo de aplicaciones Web 1.
+ */
+public class TOficina extends Trabajador{
+    
+    private String departamento;
+    private LinkedHashSet<Nomina> nominas;
+    
+    /**
+     * Constructor.
+     * Esta clase hereda de Trabajador.
+     * 
+     * @param nif - nif del trabajador. Heredado de Trabajador.
+     * @param nombre - nombre del trabajador. Heredado de Trabajador.
+     * @param apellidos - apellidos del trabajador. Heredado de Trabajador.
+     * @param telefono - teleono del trabajador. Heredado de Trabajador.
+     * @param direccion - dirección del trabajador. Heredado de Trabajador.
+     * @param departamento - departamento en el que trabaja.
+     * 
+     * @throws java.lang.Exception
+     */
+    public TOficina(String nif, String nombre, String apellidos, String telefono, String direccion, String departamento)throws Exception {
+        super(nif, nombre, apellidos, telefono, direccion);
+        this.departamento=departamento;
+        nominas=new LinkedHashSet<>();
+    }
+    
+    /**
+     * Método para generar nóminas.
+     * @param id -  id de la nómina.
+     * @param mes - mes de ésta.
+     * @param anio - año de ésta.
+     * @param cant - cantidad de la misma.
+     */
+    public void generarNomina(int id, int mes, int anio, double cant) throws Exception{
+          //Hay que mover esto hayá donde se vaya a dar la opción de generar nómina.
+        
+        Nomina n=new Nomina(id, mes, anio, cant);
+        //Instancia la nómina, y la añade a la lista de nóminas.
+        nominas.add(n);
+    } 
+    
+}
